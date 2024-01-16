79b034d1dac0cd9962bcab67ef7b8814816ae070
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-01-16 05:05:05 -0800

    DI pattern is nice.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ## GOTH STACK - Go, Templ, HTMX 
+
+## Echo
+- compatible with http/net
+Echo - func (context) error
+
+## Fiber
+- not compatible with core http/net since uses fast api
+Fiber - func (context) error
+
+Chi
+
+## Gin
+- does not return an error so no central error handling
+Gin - func(context)
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/cgradwohl/goth-stack/handler"
+	"github.com/labstack/echo/v4"
+)
+
+// mock
 
 func main() {
+	app := echo.New()
+
+	// note: here is how we can pass a DB instance to the handler as dependency injection
+	// type DB struct{}
+	// var dB DB
+	// userHandler := handler.UserHandler{db}
+
+	userHandler := handler.UserHandler{}
+	app.GET("/user", userHandler.HandleUserShow)
+
+	app.Start(":3000")
+
 	fmt.Println("hello creature ...")
 }
